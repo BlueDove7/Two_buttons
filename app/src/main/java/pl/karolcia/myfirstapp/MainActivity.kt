d6f350d65bec1954cee674cdd874920f8ded8268
WOJTEK <wojtekgol7@o2.pl>
2021-12-06 18:03:48 +0100

    view binding + activity refactor

--- app/src/main/java/pl/karolcia/myfirstapp/MainActivity.kt
+++ app/src/main/java/pl/karolcia/myfirstapp/MainActivity.kt
@@ -1,48 +1,42 @@
 package pl.karolcia.myfirstapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.util.AttributeSet
+import android.view.View
+import pl.karolcia.myfirstapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var diaperCounterMarcin = 0
+    private var diaperCounterKarolcia = 0
+
+    private val viewBiding by lazy(LazyThreadSafetyMode.NONE) {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
-       super.onCreate(savedInstanceState)  ///aplikacja idzie od zera
+        super.onCreate(savedInstanceState)  ///aplikacja idzie od zera //nie aplikacja tylko Activity, aplikacja może się składać z wielu activity
         setContentView(R.layout.activity_main)
 
-        val btnClickMe1 = findViewById<Button>(R.id.button1)
-        val btnClickMe2 = findViewById<Button>(R.id.button2)
-//btnClickMe.text = " 1 Pielucha"
-
-       /* btnClickMe1.setOnClickListener {
-            btnClickMe1.text = "HAHA, you clck"
-        }*/
-
-        val textClickMe = findViewById<TextView>(R.id.textView)
-        val textClickMe2 = findViewById<TextView>(R.id.textView2)
-        val textClickMe3 = findViewById<TextView>(R.id.textView3)
-        var licznik_pieluch_Marcina =0
-        var licznik_pieluch_Karolci =0
-        var licznik_sumy = 0
-val Karoliny_pieluchy = "Karoliny Pieluchy:   "
-        val Marcina_pieluchy = "Marcina Pieluchy:   "
-
-        btnClickMe1.setOnClickListener {
-            licznik_pieluch_Marcina = licznik_pieluch_Marcina + 1
-licznik_sumy += 1
-            textClickMe2.text = Marcina_pieluchy + licznik_pieluch_Marcina.toString()
-            textClickMe.text = licznik_sumy.toString()
-
-            Toast.makeText(this, "Hey, I am toast", Toast.LENGTH_LONG).show()
+        viewBiding.button1.setOnClickListener {
+            diaperCounterMarcin++
+            viewBiding.textView2.text = "Marcin's Diapers: $diaperCounterMarcin"
+            updateSumCounter()
         }
-        btnClickMe2.setOnClickListener {
-            licznik_pieluch_Karolci = licznik_pieluch_Karolci + 1
-            licznik_sumy += 1
-            textClickMe.text = licznik_sumy.toString()
-            textClickMe3.text =  Karoliny_pieluchy + licznik_pieluch_Karolci.toString()
-
-            Toast.makeText(this, "Hey, I am toast", Toast.LENGTH_LONG).show()
+        viewBiding.button2.setOnClickListener {
+            diaperCounterKarolcia++
+            viewBiding.textView3.text = "Karolcia's Diapers: $diaperCounterKarolcia"
+            updateSumCounter()
         }
+    }
+
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+        return viewBiding.root
+    }
+
+    private fun updateSumCounter() {
+        viewBiding.textView.text = (diaperCounterMarcin + diaperCounterKarolcia).toString()
     }
 }
